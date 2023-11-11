c30314f8138b173f73178ba18055542dcfffc3e4
pranay-23 <97128105+pranay-23@users.noreply.github.com>
2023-11-11 14:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 $("#input").change(function(){
-    let change=$(this).val();
-    $("ul").append("<li>"+ change +"<i class='fa-solid fa-check'></i><i class='fa-solid fa-trash'></i></li>");
-    $(this).val('');
+    addList();
+    saveList();
 });
+const ulist=$("ul");
+
+function addList(text="",ch=0){
+    let change="";
+    if(text===""){
+        change=$("#input").val();
+    }
+    else{
+        change=text;
+    }
+    const list=document.createElement("li");
+    list.innerHTML=`${change}<i class='fa-solid fa-check'></i><i class='fa-solid fa-trash'</i>`;
+    if(ch===1){
+        list.classList.add("checked");
+    }
+    $("#input").val('');
+    ulist.append(list);
+    saveList();
+}
 
 $("ul").on("click",".fa-trash",function(){
-    $(this).parent("li").fadeOut(300);
+    $(this).parent("li").remove();
+    saveList();
 });
 
-$("ul").on("click",".fa-check",function(){
+$("ul").on("click",".fa-check",function check(){
     $(this).parent("li").toggleClass("checked");
+    saveList();
 });
+
+function saveList(){
+    const lists=$("li");
+    const data1=[];
+    const data2=[];
+        lists.each(
+            (i)=>{
+                data1.push(lists[i].textContent)
+                if(lists[i].classList.contains("checked")){
+                    data2.push(1);
+                }else{
+                    data2.push(0);
+                }
+            }
+        );
+        if(data1.length==0){
+            localStorage.removeItem("list");
+        }else{
+            localStorage.setItem("list",JSON.stringify(data1));
+        }
+
+        if(data2.length===0){
+            localStorage.removeItem("check");
+        }else{
+            localStorage.setItem("check",JSON.stringify(data2));
+        }
+}
+
+(
+    function(){
+        const lnotes=JSON.parse(localStorage.getItem("list"));
+        const checked=JSON.parse(localStorage.getItem("check"));
+        if(lnotes!==null){
+            lnotes.forEach(
+                (lsnote,i)=>{
+                    addList(lsnote,checked[i]);
+                }
+            )
+        }
+    
+        
+    }
+)()
